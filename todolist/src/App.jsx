90d11ba603fd7e850796758c136ef469aef4686f
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-11-15 18:10:05 +0530

    basic logic added

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState('');
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    setTask('');
+
+  }
   return (
     <>
     <div>
       <h1>To-Do-List</h1>
       <div>
-        <form>
+        <form onSubmit={submithandler}>
           <input type="text" placeholder='Enter task details...' value={task} 
           onChange={(e) => {setTask(e.target.value)}}/>
+          <button>Add Task</button>
         </form>
       </div>
     </div>
