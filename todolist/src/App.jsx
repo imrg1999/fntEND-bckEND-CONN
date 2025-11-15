9226157ebeba90a251de4425bb97de963e755992
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-11-15 18:03:23 +0530

    initial state management done

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [task, setTask] = useState([]);
   return (
     <>
     <div>
       <h1>To-Do-List</h1>
       <div>
         <form>
-          <input type="text" placeholder='Enter task details...' />
+          <input type="text" placeholder='Enter task details...' value={task} 
+          onChange={(e) => {setTask(e.target.value)}}/>
         </form>
       </div>
     </div>
