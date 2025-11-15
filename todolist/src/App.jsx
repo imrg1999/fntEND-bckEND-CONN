8c13bc06436e8ab564201ac5dd3c9eed437847c7
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-11-16 02:03:02 +0530

    rendertask added

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -3,23 +3,45 @@ import { useState } from 'react'
 const App = () => {
   const [task, setTask] = useState('');
 
+  const [display, setDisplay] = useState([]);
+
   const submithandler = (e) => {
     e.preventDefault();
+    
+    setDisplay([...display,task]);
     setTask('');
+  }
 
+  const renderTask = <h1>No Tasks Available</h1>
+  if(display.length > 0) {
+    renderTask.map((todo, index) => {
+      return(
+        <li key={index}>
+          <div>
+            <h3>{todo.index+1}</h3>
+            <h2>{todo.task}</h2>
+            <button>Delete</button>
+          </div>
+        </li>
+      )
+    })
   }
   return (
     <>
     <div>
-      <h1>To-Do-List</h1>
+      <h1 className='text-7xl text-center bg-blue-400'>To-Do-List</h1>
       <div>
         <form onSubmit={submithandler}>
           <input type="text" placeholder='Enter task details...' value={task} 
-          onChange={(e) => {setTask(e.target.value)}}/>
+          onChange={(e) => {setTask(e.target.value)}} 
+          className='text-xl font-medium text-black'/>
           <button>Add Task</button>
         </form>
       </div>
     </div>
+
+    <hr />
+    <div>{renderTask}</div>
     </>
   )
 }
