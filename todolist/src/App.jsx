80b5da9b4defb669bf8c979952fda94082d572ac
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-11-18 10:23:04 +0530

    frontend updated to connect to backend

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,18 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
   const [task, setTask] = useState('');
-
   const [display, setDisplay] = useState([]);
 
-  const submithandler = (e) => {
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async() => {
+    const res = await fetch("http://localhost:3000/todo/tasks");
+    const data = await res.json();
+    setDisplay(data.tasks);
+  }
+
+
+  const submithandler = async(e) => {
     e.preventDefault();
     
-    setDisplay([...display,{task}]);
+ if (!task.trim()) return;
+
+  const res = await fetch("http://localhost:3000/todo/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ task }),
+  });
+
+  const data = await res.json();
+
+   //setDisplay([...display,data.task]);
+   fetchTasks();
     setTask('');
   }
 
-const deleteHandler = (index) => {
+const deleteHandler = async(index, id) => {
+  
+  await fetch(`http://localhost:3000/todo/delete/${id}`, {
+    method: "DELETE"
+  });
   let copyDisplay = [...display];
     copyDisplay.splice(index,1);
     setDisplay(copyDisplay)
@@ -22,11 +49,11 @@ const deleteHandler = (index) => {
   if(display.length > 0) {
    renderTask = display.map((todo, index) => {
       return(
-        <li key={index} className='flex justify-center'>
+        <li key={todo.id} className='flex justify-center'>
           <div className='flex justify-center mt-2'>
             <h3>{index+1}.   </h3>
             <h2>{todo.task}</h2>
-            <button onClick={() => {deleteHandler(index)}} 
+            <button onClick={() => {deleteHandler(index,todo.id)}} 
             className='text-xs font-medium text-black px-1 py-2 border-2 m-2 text-white bg-red-800 rounded'>
               Delete</button>
           </div>
